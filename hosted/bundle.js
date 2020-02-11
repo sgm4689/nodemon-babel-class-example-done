f3d96c59817cfe0a9d92acf773787830ef598845
Austin Willoughby <arwigm@rit.edu>
2020-02-11 18:09:16 -0500

    uploaded done version

---
+++ hosted/bundle.js
@@ -0,0 +1,61 @@
+"use strict";
+
+/**#ES6 file to be converted into an ES5 file
+    Our babel build/watch scripts in the package.json
+    will convert this into ES5 and put it into the hosted folder.
+**/
+var handleResponse = function handleResponse(xhr) {
+  var content = document.querySelector("#content");
+  var obj = JSON.parse(xhr.response);
+  console.dir(obj);
+
+  switch (xhr.status) {
+    case 200:
+      content.innerHTML = "<b>Success</b>";
+      break;
+
+    case 400:
+      content.innerHTML = "<b>Bad Request</b>";
+      break;
+
+    default:
+      content.innerHTML = "Error code not implemented by client.";
+      break;
+  }
+};
+
+var sendAjax = function sendAjax(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.setRequestHeader("Accept", 'application/json');
+
+  xhr.onload = function () {
+    return handleResponse(xhr);
+  };
+
+  xhr.send();
+};
+
+var init = function init() {
+  var successButton = document.querySelector("#success");
+  var badRequestButton = document.querySelector("#badRequest");
+  var notFoundButton = document.querySelector("#notFound");
+
+  var success = function success() {
+    return sendAjax('/success');
+  };
+
+  var badRequest = function badRequest() {
+    return sendAjax('/badRequest');
+  };
+
+  var notFound = function notFound() {
+    return sendAjax('/notFoundURL');
+  };
+
+  successButton.addEventListener('click', success);
+  badRequestButton.addEventListener('click', badRequest);
+  notFoundButton.addEventListener('click', notFound);
+};
+
+window.onload = init;
---
+++ src/server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// added route to get for our ES5 JS bundle.
+// This bundle will be created by our babel
+// watch/build scripts in package.json
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/bundle.js': htmlHandler.getBundle,
+  '/success': jsonHandler.success,
+  '/badRequest': jsonHandler.badRequest,
+  notFound: jsonHandler.notFound,
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  const params = query.parse(parsedUrl.query);
+
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, params);
+  } else {
+    urlStruct.notFound(request, response, params);
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
